b99b387664363194d011cfcd65f130853bad4040
JJJ <971622721@qq.com>
2022-06-17 11:28:09 +0800

    初始化

---
+++ 2022_6_17/main.py
@@ -0,0 +1,55 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+message = "JJJ is man"
+print(message)
+
+message = "Hwm is girl"
+print(message)
+
+message =  "'lie' is not allowed "
+print(message)
+
+message = "lie"
+print(message)
+
+print(message.upper())
+print(message.lower())
+
+first_name = "J"
+last_name = "JJ"
+full_name  = first_name + " " +last_name
+print(full_name.title())#title will set the first lettle upper
+
+print("Python")
+print("\tPython")
+print("Py\nthon")
+
+message = "python  "
+print(message)
+
+print(message.rstrip())
+
+message = message.rstrip()
+print(message)
+
+name = "Eric"
+print("Hello " + name +",would you like to learn some Python today?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+motto = 'Albert Einstein once said "Everyone is a fool"'
+print(motto)
+
+age = 20
+name = "JJJ "
+say = " Happy Birthday!"
+#print(name + age + say)
+print(name + str(age) + say)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
